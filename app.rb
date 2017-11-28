f83c5ba7734f6b991d9346c48c00978a9cb01f99
Guesly  Germain <germain1211@outlook.com>
2017-11-28 15:57:40 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -26,6 +26,7 @@ class App < Sinatra::Base
       "#{params[:number1].to_i + params[number2].to_i}"
     else
       "#{params[:number1].to_i - params[number2].to_i}"
+      binding.pry
     end
   end
 end
