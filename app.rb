f9de5193b6b9366efd1a055a68be399a9d1fed71
Guesly  Germain <germain1211@outlook.com>
2017-11-28 15:25:01 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,5 +2,17 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    "#{params[:name].reverse}"
+  end
 
+  get '/square/:number' do
+
+    "#{params[:number].to_i.**2}"
+    # binding.pry
+  end
+
+  get '/say/:number/:phrase' do
+    binding.pry
+  end
 end
