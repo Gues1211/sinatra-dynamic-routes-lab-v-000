1fcf3b616b7968d7168b329c360205be0be28c7c
Guesly  Germain <germain1211@outlook.com>
2017-11-28 15:55:46 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -23,9 +23,9 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do
 
     if params[:operatin] = "add"
-      "params[:number1].to_i + params[number2]"
+      "params[:number1].to_i + params[number2].to_i"
     else
-      "params[:number1].to_i - params[number2]"
+      "params[:number1].to_i - params[number2].to_i"
     end
   end
 end
