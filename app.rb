71bb3f9d4c1f1eaf4f743ba4fd5764495c1fc431
Guesly  Germain <germain1211@outlook.com>
2017-11-28 16:08:04 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -22,7 +22,6 @@ class App < Sinatra::Base
 
   get '/:operation/:number1/:number2' do
 
-    binding.pry
     if params[:operation] == "add"
       "#{params[:number1].to_i + params[:number2].to_i}"
     elsif params[:operation] == "subtract"
